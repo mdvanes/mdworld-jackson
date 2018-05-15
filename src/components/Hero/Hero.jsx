18a5dced8eb57f46f68258b0c6a3e9ba22aebad0
M.D. van Es <es@mdworld.nl>
2018-05-15 19:02:32 +0200

    Hero layout

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
 import {Grid, Cell} from 'react-md';
-import { withPrefix } from 'gatsby-link';
 import './Hero.scss';
-
-/*
-<div className="md-grid md-grid--no-spacing md-cell--middle mdworld-hero">
-    <div className="md-grid md-cell--5 mobile-fix">
-        <h1>MDWORLD</h1>
-        <h2>a webdevelopment blog, since 2003</h2>
-    </div>
-    <div className="md-grid md-cell--3 mobile-fix">
-        <h1>MDWORLD</h1>
-        <h2>a webdevelopment blog, since 2003</h2>
-    </div>
-</div>
-*/
+import logo from './logo.png';
 
 class Hero extends React.Component {
     render() {
         return (
           <Grid className="mdworld-hero">
-            <Cell size={6} offset={2}>
+            <Cell size={6} desktopOffset={2}>
               <h1>MDWORLD</h1>
               <h2>a webdevelopment blog, since 2003</h2>
             </Cell>
-            <Cell size={2}>
-                <img src={withPrefix('/logos/logo-192.png')} alt="Logo" />
+            <Cell size={2} tabletSize={8} phoneSize={6} className="img-wrapper">
+              <img src={logo} alt="Logo" />
             </Cell>
           </Grid>
         );
