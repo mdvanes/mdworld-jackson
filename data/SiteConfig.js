1cf3c1a8eaa97ec53ae46a313c094c33c77629f7
M.D. van Es <es@mdworld.nl>
2018-05-15 20:42:50 +0200

    Update GA tracking ID

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,14 +3,14 @@ module.exports = {
   siteTitle: "MDWorld", // Site title.
   siteTitleAlt: "MDWorld, a webdevelopment blog", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteUrl: "https://mdworld.nl", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
-  disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", // Disqus shortname.
+  // siteFBAppID: "123", // FB Application ID for using app insights
+  siteGATrackingID: "UA-9626603-1", // Tracking code ID for google analytics.
+  disqusShortname: "https-mdvanes-github-io", // Disqus shortname.
   postDefaultCategoryID: "Tech", // Default category for posts.
   userName: "M.D. van Es", // Username to display in the author segment.
   // userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
