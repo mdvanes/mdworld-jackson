3aa6fc6c514de15902c7d73d09211ecacda1aec4
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-05-21 13:46:53 +0200

    Add 404 with redirect for lift domain

---
+++ src/pages/404.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import About from "../components/About/About";
+import config from "../../data/SiteConfig";
+import Card from "react-md/lib/Cards/Card";
+import CardText from "react-md/lib/Cards/CardText";
+
+class NotFoundPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '404 Page Not Found'
+    };
+  }
+
+  componentWillMount() {
+    if(window.location.host.split('.')[0] === 'lift' || window.location.href.indexOf('lift') > -1) {
+      this.setState({message: 'You are being redirected to LIFT...'});
+      setTimeout(() => {
+        window.location.href = 'https://mdvanes.github.io/mdworld-simon/scriptie/';
+      }, 1000);
+    }
+  }
+
+  render() {
+    return (
+      <div className="about-container">
+        <Helmet>
+          <title>{`About | ${config.siteTitle}`}</title>
+          <link rel="canonical" href={`${config.siteUrl}/about/`} />
+        </Helmet>
+        <div className="md-grid mobile-fix">
+          <Card className="md-grid md-cell--8">
+            <CardText>
+              <h1>{this.state.message}</h1>
+            </CardText>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
