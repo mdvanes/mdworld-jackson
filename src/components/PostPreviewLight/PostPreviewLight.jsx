05ec8d2927f01f36e5fd3588b49415de7b5cc317
M.D. van Es <es@mdworld.nl>
2018-06-20 19:55:24 +0200

    Use jpg and webp instead of png for covers

--- src/components/PostPreviewLight/PostPreviewLight.jsx
+++ src/components/PostPreviewLight/PostPreviewLight.jsx
@@ -1,37 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Card from "react-md/lib/Cards/Card";
 import CardTitle from "react-md/lib/Cards/CardTitle";
-import Button from "react-md/lib/Buttons";
 import Avatar from "react-md/lib/Avatars";
 import FontIcon from "react-md/lib/FontIcons";
-import Media, { MediaOverlay } from "react-md/lib/Media";
 import PostPreview from "../PostPreview/PostPreview";
 import "../PostPreview/PostPreview.scss";
 
 class PostPreviewLight extends PostPreview {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     mobile: true
-  //   };
-  //   this.handleResize = this.handleResize.bind(this);
-  // }
-  // componentDidMount() {
-  //   this.handleResize();
-  //   window.addEventListener("resize", this.handleResize);
-  // }
-  //
-  // componentWillUnmount() {
-  //   window.removeEventListener("resize", this.handleResize);
-  // }
-  //
-  // handleResize() {
-  //   if (window.innerWidth >= 640) {
-  //     this.setState({ mobile: false });
-  //   } else {
-  //     this.setState({ mobile: true });
-  //   }
-  // }
   render() {
     const { postInfo } = this.props;
     const { mobile } = this.state;
