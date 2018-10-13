4fe8ee4338a1359f4e5917d67871efb7294ded46
M.D. van Es <es@mdworld.nl>
2018-10-13 14:14:19 +0200

    Add dynamic logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Grid, Cell} from 'react-md';
+import DynamicLogo from '../DynamicLogo/DynamicLogo';
 import './Hero.scss';
-import logo from './logo.png';
 
 // TODO use custom font Delicious
 class Hero extends React.Component {
@@ -10,10 +10,10 @@ class Hero extends React.Component {
           <Grid className="mdworld-hero">
             <Cell size={6} desktopOffset={2}>
               <h1>MDWORLD</h1>
-              <h2>a webdevelopment blog, since 2003</h2>
+              <h2 style={{marginLeft:'0.4rem'}}>a webdevelopment blog, since 2003</h2>
             </Cell>
             <Cell size={2} tabletSize={8} phoneSize={6} className="img-wrapper">
-              <img src={logo} alt="Logo" />
+              <DynamicLogo />
             </Cell>
           </Grid>
         );
