5f7d83c906c94ce3a5e031232d33d85aa818d966
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-10-13 14:42:13 +0200

    Dynamic logo with scss modules

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import {Grid, Cell} from 'react-md';
 import DynamicLogo from '../DynamicLogo/DynamicLogo';
-import './Hero.scss';
+import styles from './Hero.module.scss';
 
 // TODO use custom font Delicious
 class Hero extends React.Component {
     render() {
         return (
-          <Grid className="mdworld-hero">
+          <Grid className={styles.hero}>
             <Cell size={6} desktopOffset={2}>
               <h1>MDWORLD</h1>
-              <h2 style={{marginLeft:'0.4rem'}}>a webdevelopment blog, since 2003</h2>
+              <h2 style={{}}>a webdevelopment blog, since 2003</h2>
             </Cell>
-            <Cell size={2} tabletSize={8} phoneSize={6} className="img-wrapper">
+            <Cell size={2} tabletSize={8} phoneSize={6}>
               <DynamicLogo />
             </Cell>
           </Grid>
